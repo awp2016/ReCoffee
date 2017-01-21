7df64e2fc181b392b6cb8e5bea41b236c82f9621
Gabriela Strezea <strezea_gabriela@yahoo.com>
2017-01-21 11:42:30 +0200

    Search nume/locatie

--- ReCoffee/forms.py
+++ ReCoffee/forms.py
@@ -1,24 +1,28 @@
 from django import forms
 
+
 class RegisterForm(forms.Form):
-	userName = forms.CharField(label="Username")
-	firstName = forms.CharField(label="First Name")
-	lastName = forms.CharField(label="Last Name")
-	passWord = forms.CharField(label="Password", widget=forms.PasswordInput)
-	birthDay = forms.DateField(label='Birth Day', input_formats=['%Y-%m-%d'])
+    userName = forms.CharField(label="Username")
+    firstName = forms.CharField(label="First Name")
+    lastName = forms.CharField(label="Last Name")
+    passWord = forms.CharField(label="Password", widget=forms.PasswordInput)
+    birthDay = forms.DateField(label='Birth Day', input_formats=['%Y-%m-%d'])
+
 
 class LoginForm(forms.Form):
     username = forms.CharField(label="Username")
     password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
+
 class ReviewForm(forms.Form):
     text = forms.CharField(label="Leave review",
-        widget=forms.Textarea)
+                           widget=forms.Textarea)
+
 
 class FavoriteForm(forms.Form):
-	username = forms.CharField(label="Username")
-	shopprofile = forms.CharField(label="Shop")
+    username = forms.CharField(label="Username")
+    shopprofile = forms.CharField(label="Shop")
+
 
 class SearchForm(forms.Form):
-    shop_name = forms.CharField(label="Nume")
-    shop_location = forms.CharField(label="Locatie")
+    shopSearch = forms.CharField(label="Cauta")
