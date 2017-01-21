3fb2a1c5ed92fae7ae290529ec64375644271ea5
Bogdan <bogdancatrinoiu@gmail.com>
2017-01-21 15:23:57 +0200

    Merge branch 'master' into Bogdan
    # Conflicts:
    #	db.sqlite3

--- ReCoffee/models.py
+++ ReCoffee/models.py
@@ -19,8 +19,6 @@ class ShopProfile(models.Model):
         return reverse('shop_page', kwargs={'pk': self.pk})
 
 
-
-
 class UserProfile(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
--- ReCoffee/urls.py
+++ ReCoffee/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     url(r'^logout', views.logout_view, name="logout"),
     url(r'^user/(?P<pk>\d+)/$', views.user_profile,
         name="user_profile"),
-    url(r'^shop/(?P<pk>\d+)/$', views.shop_profile,
+    url(r'^shop_profile/(?P<pk>\d+)/$', views.shop_profile,
         name="shop_profile"),
-    url(r'^lista_cafenele/(?P<shopSearch>[\w\-]+)/$', views.lista_cafenele_view, name="lista_cafenele"),
+    url(r'^lista_cafenele/(?P<shopSearch>[\w\-]+)/$',
+        views.lista_cafenele_view, name="lista_cafenele"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
