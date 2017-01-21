4dfb7a08fce343576fb3956ba2aa3de346c7ad08
Bogdan <bogdancatrinoiu@gmail.com>
2017-01-21 15:40:25 +0200

    commit-c2

--- ReCoffee/views.py
+++ ReCoffee/views.py
@@ -11,15 +11,20 @@ from django.http import HttpResponseRedirect
 
 from . import models
 from models import UserProfile
+from models import ShopProfile
 from . import forms
 import json
 
 
 def index(request):
-    #modificare by bogdan
-    mn = ShopProfile.filter(
-    #end modificare bogdan
     context = {}
+
+    #modificare by bogdan
+    rate = ShopProfile.objects.all().order_by('rating').reverse()
+    context['rate'] = rate
+    print rate
+
+    #end modificare bogdan
 
     if request.method == 'GET':
         form = forms.SearchForm()
